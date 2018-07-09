913558ae951eafe27531eb4feefae1f60d6735d6
Mrlu <370198370@qq.com>
2018-07-09 14:52:15 +0800

    [update] uwigi supervisor

--- app/config.py
+++ app/config.py
@@ -6,22 +6,24 @@ class Config(object):
     DB_PASSWORD = ''  # type: str
     DB_HOST = 'localhost'  # type: str
     DB_DB = 'flask-pyjwt-auth'  # type: str
-    DEBUG = True  # type: bool
-    PORT = 5002  # type: int
-    HOST = "127.0.0.1"  # type: str
+    DEBUG = False  # type: bool
+    PORT = 8080  # type: int
+    HOST = "0.0.0.0"  # type: str
     SECRET_KEY = "my blog"  # type: str
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # type: # bool
     SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_HOST + '/' + DB_DB
+    ENV = 'Production'
 
 
 class ProductionConfig(Config):
-    PORT = 8080  # type: int
-    HOST = "0.0.0.0"  # type: str
     DATABASE_URI = 'mysql://user@localhost/foo'
 
 
 class DevelopmentConfig(Config):
+    PORT = 5002  # type: int
+    HOST = "127.0.0.1"  # type: str
     DEBUG = True
+    ENV = 'Development'
 
 
 class TestingConfig(Config):
